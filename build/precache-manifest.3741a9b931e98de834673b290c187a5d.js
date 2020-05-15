0b241fc8bd565f10ccf7f5201dab32a9d2fc14a3
Jose <jozylopez27@gmail.com>
2020-05-14 20:16:07 -0500

    deploy scripts

---
+++ build/precache-manifest.3741a9b931e98de834673b290c187a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4fc2f0c8353e7669dfe72b51ec366b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd19b10dc6e94ae74bcc",
+    "url": "/static/css/main.816234fe.chunk.css"
+  },
+  {
+    "revision": "ae2dd799e3d1685fe371",
+    "url": "/static/js/2.1dcc67c3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.1dcc67c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd19b10dc6e94ae74bcc",
+    "url": "/static/js/main.f0a65509.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  },
+  {
+    "revision": "56c175e3e20744553dfd9b155ec414f5",
+    "url": "/static/media/dogPainting.56c175e3.jpg"
+  }
+]);
